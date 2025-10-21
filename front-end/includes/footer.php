c16e815094ea1c4ab596cc7f85e8f44fc5654efd
britoscar1 <brito.oscar@uabc.edu.mx>
2025-10-20 23:51:25 -0700

    Inclui header y footer

---
+++ front-end/includes/footer.php
@@ -0,0 +1,17 @@
+<footer class="footer-simpinna">
+  <div class="footer-container">
+    
+    <div class="footer-logo">
+      <img src="/SIMPINNA/front-end/assets/img/global/tkt-pueblo-magico.png" 
+           alt="Tecate Pueblo Mágico" class="logo-tecate">
+    </div>
+
+    <div class="footer-text">
+      <p>
+        ©SIMPINNA | Sistema Municipal de Protección Integral de los Derechos de las<br>
+        Niñas, Niños y Adolescentes
+      </p>
+    </div>
+
+  </div>
+</footer>
---
+++ front-end/includes/header.php
@@ -0,0 +1,23 @@
+<header class="header">
+  <div class="header-izq">
+      <img src="/SIMPINNA/front-end/assets/img/global/logo-simpinna.png" 
+           alt="Logo SIMPINNA" class="logo-simpinna">
+
+    <span class="titulo-header">
+        <a href="/SIMPINNA/front-end/frames/inicio/inicio.php">Inicio</a>
+    </span>
+  </div>
+
+  <nav class="header-nav">
+      <ul>    
+          <li><a href="/SIMPINNA/front-end/frames/encuesta/encuesta.php">Encuesta</a></li>
+          <li><a href="/SIMPINNA/front-end/frames/contacto/contacto.php">Contáctanos</a></li>
+          <li><a href="/SIMPINNA/front-end/frames/ayuda/ayuda.php">Ayuda</a></li>
+      </ul>
+  </nav>
+
+  <div class="header-der">
+      <img src="/SIMPINNA/front-end/assets/img/global/gobierno-logo.png" 
+           alt="Logo Gobierno Tecate" class="logo-gobierno">
+  </div>
+</header>
