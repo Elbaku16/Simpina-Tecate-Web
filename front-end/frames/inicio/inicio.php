d07762f9815d6ae33602e4cb768335685d4f81f6
Lenin Cortez <lenin.cortez@uabc.edu.mx>
2025-10-21 14:51:52 -0700

    Agregue el inicio

--- front-end/frames/inicio/inicio.php
+++ front-end/frames/inicio/inicio.php
@@ -9,21 +9,34 @@
     <title>SIMPINNA</title>
 </head>
 <body>
-    <header>
+    <header> 
         <?php include($_SERVER['DOCUMENT_ROOT'] . '/SIMPINNA/front-end/includes/header.php'); ?>
     </header>
     <main>
-
-    
-
-
-
+        <section class="hero-section">
+            <div class="hero-content">
+                <div class="hero-left">
+                    <img src="/SIMPINNA/front-end/assets/img/ninos-ilustracion.png" alt="Niños felices" class="hero-image">
+                    <p class="hero-text">
+                        Protegiendo los derechos y el bienestar de las niñas,<br>
+                        niños y adolescentes de Tecate, con acciones que<br>
+                        construyen un futuro seguro e inclusivo.
+                    </p>
+                </div>
+                
+                <div class="hero-right">
+                    <div class="encuesta-card">
+                        <h2>¿Conoces tus derechos?</h2>
+                        <p>¡Pongamoslo a prueba contestando las siguientes preguntas!</p>
+                        
+                        <img src="/SIMPINNA/front-end/assets/img/encuesta-icon.png" alt="Encuesta" class="encuesta-icon">
+                        
+                        <a href="/SIMPINNA/front-end/frames/encuesta.php" class="btn-encuesta">Encuesta</a>
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
-
-
-
-
-
 
     <footer>
         <?php include($_SERVER['DOCUMENT_ROOT'] . '/SIMPINNA/front-end/includes/footer.php'); ?>
