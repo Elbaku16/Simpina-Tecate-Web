f9cbf3423740d7d0464b4d7c637931df08e05977
britoscar1 <brito.oscar@uabc.edu.mx>
2025-10-21 11:24:44 -0700

    Header Actualizado

---
+++ front-end/frames/panel-admin/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://framework-gb.cdn.gob.mx/gm/v3/assets/styles/main.css">
+    <link rel="stylesheet" href="/SIMPINNA/front-end/assets/css/inicio.css">
+    <title>Iniciar Sesion</title>
+</head>
+<body>
+    <header>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/SIMPINNA/front-end/includes/header-admin.php'); ?>
+    </header>
+    <main>
+
+    
+
+
+
+    </main>
+
+
+
+
+
+
+    <footer>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/SIMPINNA/front-end/includes/footer.php'); ?>
+    </footer>
+</body>
+</html>
---
+++ front-end/includes/header-admin.php
@@ -0,0 +1,17 @@
+<header class="header">
+  <div class="header-izq">
+      <img src="/SIMPINNA/front-end/assets/img/global/logo-simpinna.png" 
+           alt="Logo SIMPINNA" class="logo-simpinna">
+
+    <span class="titulo-header">
+        <a href="/SIMPINNA/front-end/frames/inicio/inicio.php">Panel Administrativo</a>
+    </span>
+  </div>
+
+
+
+  <div class="header-der">
+      <img src="/SIMPINNA/front-end/assets/img/global/gobierno-logo.png" 
+           alt="Logo Gobierno Tecate" class="logo-gobierno">
+  </div>
+</header>
