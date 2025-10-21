ff9fba4474dd0366b99f7011f477b181baf0cce0
britoscar1 <brito.oscar@uabc.edu.mx>
2025-10-21 11:24:44 -0700

    Header Actualizado

--- front-end/includes/header.php
+++ front-end/includes/header.php
@@ -1,19 +1,40 @@
+<?php
+  $currentPage = $_SERVER['REQUEST_URI'];
+?>
+
 <header class="header">
   <div class="header-izq">
       <img src="/SIMPINNA/front-end/assets/img/global/logo-simpinna.png" 
            alt="Logo SIMPINNA" class="logo-simpinna">
-
-    <span class="titulo-header">
-        <a href="/SIMPINNA/front-end/frames/inicio/inicio.php">Inicio</a>
-    </span>
   </div>
 
   <nav class="header-nav">
-      <ul>    
-          <li><a href="/SIMPINNA/front-end/frames/encuesta/encuesta.php">Encuesta</a></li>
-          <li><a href="/SIMPINNA/front-end/frames/contacto/contacto.php">Contáctanos</a></li>
-          <li><a href="/SIMPINNA/front-end/frames/ayuda/ayuda.php">Ayuda</a></li>
-      </ul>
+    <ul>    
+        <li>
+          <a href="/SIMPINNA/front-end/frames/inicio/inicio.php"
+             class="<?php echo (strpos($currentPage, 'inicio.php') !== false) ? 'active' : ''; ?>">
+             Inicio
+          </a>
+        </li>
+        <li>
+          <a href="/SIMPINNA/front-end/frames/encuesta/encuesta.php"
+             class="<?php echo (strpos($currentPage, 'encuesta.php') !== false) ? 'active' : ''; ?>">
+             Encuesta
+          </a>
+        </li>
+        <li>
+          <a href="/SIMPINNA/front-end/frames/contacto/contacto.php"
+             class="<?php echo (strpos($currentPage, 'contacto.php') !== false) ? 'active' : ''; ?>">
+             Contáctanos
+          </a>
+        </li>
+        <li>
+          <a href="/SIMPINNA/front-end/frames/ayuda/ayuda.php"
+             class="<?php echo (strpos($currentPage, 'ayuda.php') !== false) ? 'active' : ''; ?>">
+             Ayuda
+          </a>
+        </li>
+    </ul>
   </nav>
 
   <div class="header-der">
