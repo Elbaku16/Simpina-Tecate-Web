072e97218a1523b2eb68dd8c2e2599429b45b552
Lenin Cortez <lenin.cortez@uabc.edu.mx>
2025-10-23 00:07:21 -0700

    Inicio amigable

--- front-end/frames/inicio/inicio.php
+++ front-end/frames/inicio/inicio.php
@@ -5,9 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://framework-gb.cdn.gob.mx/gm/v3/assets/styles/main.css">
     <link rel="stylesheet" href="/SIMPINNA/front-end/assets/css/inicio.css">
-        <link rel="stylesheet" href="/SIMPINNA/front-end/assets/css/lenin.css">
-
-
+    <link rel="stylesheet" href="/SIMPINNA/front-end/assets/css/lenin.css">
     <title>SIMPINNA</title>
 </head>
 <body>
@@ -18,10 +16,10 @@
         <section class="hero-section">
             <div class="hero-content">
                 <div class="hero-left">
-                    <img src="/SIMPINNA/front-end/assets/img/ninos-ilustracion.png" alt="Niños felices" class="hero-image">
+                    <img src="/SIMPINNA/front-end/assets/img/inicio/ninos.png?v=1" alt="Niños felices" class="hero-image">
                     <p class="hero-text">
-                        Protegiendo los derechos y el bienestar de las niñas,<br>
-                        niños y adolescentes de Tecate, con acciones que<br>
+                        Protegiendo los derechos y el bienestar de las niñas,
+                        niños y adolescentes de Tecate, con acciones que
                         construyen un futuro seguro e inclusivo.
                     </p>
                 </div>
@@ -30,10 +28,8 @@
                     <div class="encuesta-card">
                         <h2>¿Conoces tus derechos?</h2>
                         <p>¡Pongamoslo a prueba contestando las siguientes preguntas!</p>
-                        
-                        <img src="/SIMPINNA/front-end/assets/img/encuesta-icon.png" alt="Encuesta" class="encuesta-icon">
-                        
-                        <a href="/SIMPINNA/front-end/frames/encuesta.php" class="btn-encuesta">Encuesta</a>
+                        <img src="/SIMPINNA/front-end/assets/img/inicio/tablas.png?v=1" alt="Encuesta" class="encuesta-icon">
+                        <a href="/SIMPINNA/front-end/pages/encuesta.php" class="btn-encuesta">Encuesta</a>
                     </div>
                 </div>
             </div>
@@ -43,6 +39,5 @@
     <footer>
         <?php include($_SERVER['DOCUMENT_ROOT'] . '/SIMPINNA/front-end/includes/footer.php'); ?>
     </footer>
-
 </body>
 </html>
