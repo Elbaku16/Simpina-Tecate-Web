acb1863902f2cfc64ee1a1d443f09494416f7e0a
britoscar1 <brito.oscar@uabc.edu.mx>
2025-11-05 13:04:05 -0800

    Mejoras de diseno e interactividad (#3)
    * Mejoras en diseno

--- front-end/frames/inicio/seleccion-encuesta.php
+++ front-end/frames/inicio/seleccion-encuesta.php
@@ -14,40 +14,39 @@
   <?php include $_SERVER['DOCUMENT_ROOT'].'/SIMPINNA/front-end/includes/header.php'; ?>
 </header>
 
-<section class="container escolaridad">
-
-  <div class="strip">
-    <div>
-      <h3 class="title">Elige tu escolaridad:</h3>
-      <h6 class="subtitle">Toca el botón correcto o pídele ayuda a tu maestro.</h6>
+<section class="survey-selection">
+  <div class="survey-selection__wrapper">
+    <div class="survey-selection__header">
+      <h3 class="title">Elige tu escolaridad</h3>
+      <h6 class="subtitle">Selecciona el nivel educativo para comenzar. Si lo necesitas, solicita apoyo a tu docente.</h6>
     </div>
+
+    <ul class="grade-grid">
+      <li class="grade-item preescolar">
+        <img src="/SIMPINNA/front-end/assets/img/escolaridad/preescolar.png" alt="Preescolar">
+        <h3>Preescolar</h3>
+        <a href="/SIMPINNA/front-end/frames/encuestas/demo-encuestas.php?nivel=preescolar" class="btn-nivel nivel--green">Comenzar</a>
+      </li>
+
+      <li class="grade-item primaria">
+        <img src="/SIMPINNA/front-end/assets/img/escolaridad/primaria.png" alt="Primaria">
+        <h3>Primaria</h3>
+        <a href="/SIMPINNA/front-end/frames/encuestas/demo-encuestas.php?nivel=primaria" class="btn-nivel nivel--blue">Comenzar</a>
+      </li>
+
+      <li class="grade-item secundaria">
+        <img src="/SIMPINNA/front-end/assets/img/escolaridad/secundaria.png" alt="Secundaria">
+        <h3>Secundaria</h3>
+        <a href="/SIMPINNA/front-end/frames/encuestas/demo-encuestas.php?nivel=secundaria" class="btn-nivel nivel--red">Comenzar</a>
+      </li>
+
+      <li class="grade-item preparatoria">
+        <img src="/SIMPINNA/front-end/assets/img/escolaridad/preparatoria.png" alt="Preparatoria">
+        <h3>Preparatoria</h3>
+        <a href="/SIMPINNA/front-end/frames/encuestas/demo-encuestas.php?nivel=preparatoria" class="btn-nivel nivel--magenta">Comenzar</a>
+      </li>
+    </ul>
   </div>
-
-  <ul class="grade-grid">
-    <li class="grade-item preescolar">
-      <img src="/SIMPINNA/front-end/assets/img/escolaridad/preescolar.png" alt="Preescolar">
-      <h3>Preescolar</h3>
-      <a href="/SIMPINNA/front-end/frames/encuestas/demo-encuestas.php?nivel=preescolar" class="btn-nivel nivel--green">Comenzar</a>
-    </li>
-
-    <li class="grade-item primaria">
-      <img src="/SIMPINNA/front-end/assets/img/escolaridad/primaria.png" alt="Primaria">
-      <h3>Primaria</h3>
-      <a href="/SIMPINNA/front-end/frames/encuestas/demo-encuestas.php?nivel=primaria" class="btn-nivel nivel--blue">Comenzar</a>
-    </li>
-
-    <li class="grade-item secundaria">
-      <img src="/SIMPINNA/front-end/assets/img/escolaridad/secundaria.png" alt="Secundaria">
-      <h3>Secundaria</h3>
-      <a href="/SIMPINNA/front-end/frames/encuestas/demo-encuestas.php?nivel=secundaria" class="btn-nivel nivel--red">Comenzar</a>
-    </li>
-
-    <li class="grade-item preparatoria">
-      <img src="/SIMPINNA/front-end/assets/img/escolaridad/preparatoria.png" alt="Preparatoria">
-      <h3>Preparatoria</h3>
-      <a href="/SIMPINNA/front-end/frames/encuestas/demo-encuestas.php?nivel=preparatoria" class="btn-nivel nivel--magenta">Comenzar</a>
-    </li>
-  </ul>
 </section>
 
 <footer>
