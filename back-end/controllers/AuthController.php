d18367c33b19ce0f11d0a15c4e1e17eb1348d62d
britoscar1 <brito.oscar@uabc.edu.mx>
2025-11-16 11:08:39 -0800

    Refactorizacion1

---
+++ back-end/controllers/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * AuthController
+ *
+ * TODO Codex:
+ * - Implementar clase AuthController con métodos:
+ *      login(array $input)
+ *      logout()
+ * - NO generar HTML (solo lógica)
+ * - NO hacer echo ni print
+ * - Mover aquí toda la lógica que hoy vive en login.php y logout.php
+ *
+ * Reglas:
+ * - Usar verificar-sesion.php para CSRF y sesiones
+ * - Usar conexion-db.php para BD
+ * - Producir resultado booleano + mensajes de error
+ */
+class AuthController {
+
+}
---
+++ back-end/models/UsuarioAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * TODO: Modelo para la tabla usuarios_admin.
+ *
+ * Tabla:
+ * - id_admin (int, PK)
+ * - usuario (varchar)
+ * - password (varchar o hashed)
+ *
+ * Responsabilidades:
+ * - Buscar usuario por nombre de usuario.
+ * - Verificar contraseña (según esquema actual).
+ */
+class UsuarioAdmin
+{
+    // TODO: propiedades privadas de la entidad
+    // TODO: constructor opcional
+
+    /**
+     * TODO: Obtener un admin por su nombre de usuario.
+     * Firma sugerida:
+     *   public static function findByUsername(mysqli $conn, string $usuario): ?UsuarioAdmin
+     */
+}
