283f567e17951b3dcdbf97b62fecb310b4c7a9b2
britoscar1 <brito.oscar@uabc.edu.mx>
2025-11-27 23:19:12 -0800

    Cambios MUY pequenos

--- front-end/frames/panel/panel-admin.php
+++ front-end/frames/panel/panel-admin.php
@@ -220,7 +220,7 @@ requerir_admin();
       
       <?php if (rol_es('admin')): ?>
       <div class="comentarios-box" style="margin-top: 1.5rem; text-align: center;">
-        <div class="comentarios-icon" style="background: linear-gradient(135deg, #4A0E26, #2D1B1F);">
+        <div class="comentarios-icon">
             <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M19 21v-2a4 4 0 00-4-4H9a4 4 0 00-4 4v2"></path>
                 <circle cx="12" cy="7" r="4"></circle>
